c29abb0feb02a30c69b32a832088aed98da7fe25
Herbert Bergauer <bergauer@gtmtca2.hephy.oeaw.ac.at>
2018-11-27 15:57:23 +0100

    updated output register

--- firmware/hdl/gt_mp7_core/gtl_fdl_wrapper/gtl/comparators.vhd
+++ firmware/hdl/gt_mp7_core/gtl_fdl_wrapper/gtl/comparators.vhd
@@ -20,7 +20,7 @@ end comparators;
 architecture rtl of comparators is
 begin
 
-    constant OUT_REG_WIDTH : positive := (CONF.N_OBJ_1_H +1) * (CONF.N_OBJ_2_H + 1);
+    constant OUT_REG_WIDTH : positive := 1;
     type std_logic_vector_2dim is array(CONF.N_OBJ_1_H downto 0)(CONF.N_OBJ_2_H downto 0) of std_logic_Vector(CONF.C_WIDTH-1 downto 0);
     signal data_vec : std_logic_vector_2dim(CONF.N_OBJ_1_H downto 0)CONF.(N_OBJ_2_H downto 0);
     signal comp : std_logic_2dim(CONF.N_OBJ_1_H downto 0)(CONF.N_OBJ_2_H downto 0);
@@ -28,24 +28,23 @@ begin
     l1: for i in 0 to CONF.N_OBJ_1_H generate
         l2: for j in 0 to CONF.N_OBJ_2_H generate
             l3: for k in 0 to CONF.C_WIDTH-1 generate
-                data_vec(i)(j)(k) <= data(i)(j)(k);
+                data_vec(i,j)(k) <= data(i,j)(k);
             end generate l3;
             if_m: if CONF.GE_MODE generate
                 if_w: if CONF.WINDOW generate
-                comp => (data_vec(i)(j) >= REQ_L) and (data_vec(i)(j) <= REQ_H);
+                comp(i,j) => (data_vec(i,j) >= REQ_L) and (data_vec(i,j) <= REQ_H);
                 end generate;
                 if_n_w: if not WINDOW generate
-                comp => data_vec(i)(j) >= REQ_L;
+                comp(i,j) => data_vec(i,j) >= REQ_L;
                 end generate;
             end generate;
             if_n_m: if not GE_MODE generate
-                comp => data_vec(i)(j) = REQ_L;
+                comp(i,j) => data_vec(i,j) = REQ_L;
             end generate;
+            out_reg_i : entity work.out_reg_mux
+                generic map(OUT_REG_WIDTH, CONF.OUT_REG);  
+                port map(clk, comp(i,j), comp_o(i,j)); 
         end generate l2;
     end generate l1;
 
-    out_reg_i : entity work.out_reg_mux
-        generic map(OUT_REG_WIDTH, CONF.OUT_REG);  
-        port map(clk, comp, comp_o); 
-    
 end architecture rtl;
--- firmware/hdl/gt_mp7_core/gtl_fdl_wrapper/gtl/difference_eta.vhd
+++ firmware/hdl/gt_mp7_core/gtl_fdl_wrapper/gtl/difference_eta.vhd
@@ -28,13 +28,16 @@ entity difference_eta is
         in_1 : in diff_integer_inputs_array(0 to CONF.NR_OBJ_1-1);
         in_2 : in diff_integer_inputs_array(0 to CONF.NR_OBJ_2-1);
         diff_vector_o : out deta_dphi_vector_array(0 to CONF.NR_OBJ_1-1, 0 to CONF.NR_OBJ_2-1) := (others => (others => (others => '0')));
-        cosh_deta_vector : out cosh_cos_vector_array(0 to CONF.NR_OBJ_1-1, 0 to CONF.NR_OBJ_2-1) := (others => (others => (others => '0')));
+        cosh_deta_vector_o : out cosh_cos_vector_array(0 to CONF.NR_OBJ_1-1, 0 to CONF.NR_OBJ_2-1) := (others => (others => (others => '0')));
     );
 end difference_eta;
 
 architecture rtl of difference_eta is
 
     signal diff_i : dim2_max_eta_range_array(0 to CONF.NR_OBJ_1-1, 0 to CONF.NR_OBJ_2-1);
+    signal diff_vector_i : deta_dphi_vector_array(0 to CONF.NR_OBJ_1-1, 0 to CONF.NR_OBJ_2-1);
+    signal cosh_deta_vector_i : cosh_cos_vector_array(0 to CONF.NR_OBJ_1-1, 0 to CONF.NR_OBJ_2-1);
+    constant COSH_COS_VECTOR_WIDTH : positive;
     
 begin
 
@@ -43,23 +46,27 @@ begin
 -- only positive difference in eta
             diff_i(i,j) <= abs(in_1(i) - in_2(j));
             calo_calo_i: if (CONF.OBJ_CORR = calo_calo) generate
-                diff_vector(i,j) <= CONV_STD_LOGIC_VECTOR(CALO_CALO_LUT(diff_i(i,j)),DETA_DPHI_VECTOR_WIDTH_ALL);
-                cosh_deta_vector(i,j) <= CONV_STD_LOGIC_VECTOR(CALO_CALO_COSH_COS_LUT(diff_i(i,j)), CALO_CALO_COSH_COS_VECTOR_WIDTH);
+                diff_vector_i(i,j) <= CONV_STD_LOGIC_VECTOR(CALO_CALO_LUT(diff_i(i,j)), DETA_DPHI_VECTOR_WIDTH_ALL);
+                cosh_deta_vector_i(i,j) <= CONV_STD_LOGIC_VECTOR(CALO_CALO_COSH_COS_LUT(diff_i(i,j)), CALO_CALO_COSH_COS_VECTOR_WIDTH);
+                COSH_COS_VECTOR_WIDTH <= CALO_CALO_COSH_COS_VECTOR_WIDTH;
             end generate calo_calo_i;
             calo_muon_i: if (CONF.OBJ_CORR = calo_muon) generate
-                diff_vector(i,j) <= CONV_STD_LOGIC_VECTOR(CALO_MUON_LUT(diff_i(i,j)),DETA_DPHI_VECTOR_WIDTH_ALL);
-                cosh_deta_vector(i,j) <= CONV_STD_LOGIC_VECTOR(CALO_MUON_COSH_COS_LUT(diff_i(i,j)), CALO_MUON_COSH_COS_VECTOR_WIDTH);
-            end generate calo_calo_i;
+                diff_vector_i(i,j) <= CONV_STD_LOGIC_VECTOR(CALO_MUON_LUT(diff_i(i,j)), DETA_DPHI_VECTOR_WIDTH_ALL);
+                cosh_deta_vector_i(i,j) <= CONV_STD_LOGIC_VECTOR(CALO_MUON_COSH_COS_LUT(diff_i(i,j)), CALO_MUON_COSH_COS_VECTOR_WIDTH);
+                COSH_COS_VECTOR_WIDTH <= CALO_MUON_COSH_COS_VECTOR_WIDTH;
+            end generate calo_muon_i;
             muon_muon_i: if (CONF.OBJ_CORR = muon_muon) generate
-                diff_vector(i,j) <= CONV_STD_LOGIC_VECTOR(MUON_MUON_LUT(diff_i(i,j)),DETA_DPHI_VECTOR_WIDTH_ALL);
-                cosh_deta_vector(i,j) <= CONV_STD_LOGIC_VECTOR(MUON_MUON_COSH_COS_LUT(diff_i(i,j)), MUON_MUON_COSH_COS_VECTOR_WIDTH);
-            end generate calo_calo_i;
+                diff_vector_i(i,j) <= CONV_STD_LOGIC_VECTOR(MUON_MUON_LUT(diff_i(i,j)), DETA_DPHI_VECTOR_WIDTH_ALL);
+                cosh_deta_vector_i(i,j) <= CONV_STD_LOGIC_VECTOR(MUON_MUON_COSH_COS_LUT(diff_i(i,j)), MUON_MUON_COSH_COS_VECTOR_WIDTH);
+                COSH_COS_VECTOR_WIDTH <= MUON_MUON_COSH_COS_VECTOR_WIDTH;
+            end generate muon_muon_i;
+            out_reg_diff_i : entity work.out_reg_mux
+                generic map(DETA_DPHI_VECTOR_WIDTH_ALL, CONF.OUT_REG);  
+                port map(clk, diff_vector_i(i,j), diff_vector_o(i,j)); 
+            out_reg_cosh_deta_i : entity work.out_reg_mux               
+                generic map(COSH_COS_VECTOR_WIDTH, CONF.OUT_REG);  
+                port map(clk, cosh_deta_vector_i(i,j), cosh_deta_vector_o(i,j)); 
         end generate loop_2;
     end generate loop_1;
     
--- -- noch anpassen!!!
---     out_reg_i : entity work.out_reg_mux
---         generic map(OUT_REG_WIDTH, CONF.OUT_REG);  
---         port map(clk, diff_vector, diff_vector_o); 
-
 end architecture rtl;
